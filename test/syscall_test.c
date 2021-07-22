b258f0b7dcc5f952943d70ba94bba6c143647367
dcamarmas <diego.camarmas.alonso@gmail.com>
2021-07-22 11:02:10 +0200

    fix stat syscall

--- test/syscall_test.c
+++ test/syscall_test.c
@@ -9,6 +9,7 @@
 #include <sys/unistd.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #define MAXIM (1024*1024)
 
@@ -25,7 +26,7 @@ int main()
 	fd1 = unlink("/xpn1/jj1.txt");
 	printf("xpn_unlink jj = %d\n",fd1);*/
 	
-	fd1 = creat("xpn:///xpn1/jj.txt",00777);
+	fd1 = creat("xpn:///xpn1/jj.txt",00747);
 	printf("xpn_creat jj = %d\n",fd1);
 	
 	fd2 = creat("xpn:///xpn1/jj1.txt",0777);
@@ -60,6 +61,20 @@ int main()
 	close(fd1);
 
 
-	struct stat stat_buf;
-	printf("%d\n", stat("xpn:///xpn1/jj.txt", &stat_buf));
+	struct stat file_stats;
+	stat("xpn:///xpn1/jj.txt", &file_stats);
+
+	printf(" device: %lld\n",                       file_stats.st_dev);
+	printf(" inode: %ld\n",                         file_stats.st_ino);
+	printf(" protection: %o\n",                     file_stats.st_mode);
+	printf(" number of hard links: %d\n",           file_stats.st_nlink);
+	printf(" user ID of owner: %d\n",               file_stats.st_uid);
+	printf(" group ID of owner: %d\n",              file_stats.st_gid);
+	printf(" device type (if inode device): %lld\n",file_stats.st_rdev);
+	printf(" total size, in bytes: %ld\n",          file_stats.st_size);
+	printf(" blocksize for filesystem I/O: %ld\n",  file_stats.st_blksize);
+	printf(" number of blocks allocated: %ld\n",    file_stats.st_blocks);
+	printf(" time of last access: %ld : %s",        file_stats.st_atime, ctime(&file_stats.st_atime));
+	printf(" time of last modification: %ld : %s",  file_stats.st_mtime, ctime(&file_stats.st_mtime));
+	printf(" time of last change: %ld : %s",        file_stats.st_ctime, ctime(&file_stats.st_ctime));
 }
