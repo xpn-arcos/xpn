9d0dc50282fbf64cf927417cf43f216901ac925b
dcamarmas <diego.camarmas.alonso@gmail.com>
2021-11-11 15:27:09 +0100

    minor update

--- external-utils/myServer_1.0.beta/myServer_comm.h
+++ external-utils/myServer_1.0.beta/myServer_comm.h
@@ -11,8 +11,8 @@
 
 
 void myServer_comm_reg(char *server, char *newserver, int *port);
-int myServer_comm_writedata(int fd, char *data, int size, char *id);
-int myServer_comm_readdata(int fd, char *data, int size, char *id);
+ssize_t myServer_comm_writedata(int fd, char *data, ssize_t size, char *id);
+ssize_t myServer_comm_readdata(int fd, char *data, ssize_t size, char *id);
 int myServer_comm_init(char *name, int port, char *file);	
 int myServer_accept_comm();
 void myServer_close_comm();
