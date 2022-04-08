51e5c00a619ed3a9f6966678fb348b925c93b335
Alejandro <acaldero@inf.uc3m.es>
2022-04-08 16:29:12 +0200

    mfs: minor updates

--- include/base/files_posix.h
+++ include/base/files_posix.h
@@ -1,42 +1,18 @@
 
-/*
- *  Copyright 2020-2022 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos
- *
- *  This file is part of Expand.
- *
- *  Expand is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Expand is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with Expand.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+#ifndef _FILES_POSIX_H_
+#define _FILES__POSIXH_
 
-#ifndef __FILES_POSIX_H__
-#define __FILES_POSIX_H__
+     #include "all_system.h"
+     #include "mpiServer_params.h"
+     #include <sys/stat.h>
+     #include <sys/types.h>
 
-    // Includes
-    #include "base_lib.h"
+     int  files_posix_open         ( char *pathname, int flags ) ;
+     int  files_posix_creat        ( mpiServer_param_st *params, char *pathname, mode_t mode ) ;
+     int  files_posix_close        ( int fd ) ;
 
-    #include <sys/stat.h>
-    #include <sys/mman.h>
-
-
-    // API
-    int  file_posix_init     ( void ) ;
-    int  file_posix_finalize ( void ) ;
-
-    int  file_posix_open  ( const char *path_name, int flags ) ;
-    int  file_posix_close ( int fd ) ;
-    int  file_posix_read  ( int fd, void *buffer, int buffer_size ) ;
-    int  file_posix_write ( int fd, void *buffer, int buffer_size ) ;
+     long files_posix_read_buffer  ( mpiServer_param_st *params, int read_fd2,  void *buffer, int buffer_size ) ;
+     long files_posix_write_buffer ( mpiServer_param_st *params, int write_fd2, void *buffer, int buffer_size, int num_readed_bytes ) ;
 
 #endif
 
