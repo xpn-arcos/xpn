03c62d9d2cbd84f449c5b05edb44603d0a7b8038
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2022-04-09 20:34:31 +0200

    filesystem.h: minor updates

--- include/nfi/nfi_local/nfi_local.h
+++ include/nfi/nfi_local/nfi_local.h
@@ -2,10 +2,11 @@
 #define _NFI_LOCAL_H
 
 
-#include "all_system.h"
-#include "base/string_misc.h"
-#include "nfi/nfi.h"
-#include "nfi/nfi_local/nfi_local_err.h"
+     #include "all_system.h"
+     #include "base/string_misc.h"
+     #include "base/filesystem.h"
+     #include "nfi/nfi.h"
+     #include "nfi/nfi_local/nfi_local_err.h"
 
 
  #ifdef  __cplusplus
@@ -28,37 +29,27 @@ struct nfi_local_fhandle{
 int nfi_local_init(char *url, struct nfi_server *serv, struct nfi_attr_server *attr);
 int nfi_local_destroy(struct nfi_server *server);
 
-
-
 int nfi_local_reconnect(struct nfi_server *server);
 int nfi_local_disconnect(struct nfi_server *server);
 
 int nfi_local_getattr(struct nfi_server *server, struct nfi_fhandle *fh, struct nfi_attr *attr);
-
 int nfi_local_setattr(struct nfi_server *server, struct nfi_fhandle *fh, struct nfi_attr *attr);
 
 int nfi_local_open(struct nfi_server *server, char *url, struct nfi_fhandle *fho);
-
 int nfi_local_close(struct nfi_server *server, struct nfi_fhandle *fh);
 
 ssize_t nfi_local_read(struct nfi_server *server, struct nfi_fhandle *fh, void *buffer, off_t offset, size_t size);
-
 ssize_t nfi_local_write(struct nfi_server *server, struct nfi_fhandle *fh, void *buffer, off_t offset, size_t size);
 
 int nfi_local_create(struct nfi_server *server, char *url,  struct nfi_attr *attr, struct nfi_fhandle  *fh);
-
 int nfi_local_remove(struct nfi_server *server, char *url);
-
 int nfi_local_rename(struct nfi_server *server, char *old_url, char *new_url);
 
 int nfi_local_mkdir(struct nfi_server *server, char *url, struct nfi_attr *attr, struct nfi_fhandle *fh);
-
 int nfi_local_rmdir(struct nfi_server *server, char *url);
 
 int nfi_local_opendir(struct nfi_server *server, char *url, struct nfi_fhandle *fho);
-
 int nfi_local_readdir(struct nfi_server *server, struct nfi_fhandle *fhd, char *entry , unsigned char *type);
-
 int nfi_local_closedir(struct nfi_server *server, struct nfi_fhandle *fh);
 
 int nfi_local_statfs(struct nfi_server *server, struct nfi_info *inf);
@@ -70,3 +61,4 @@ int nfi_local_statfs(struct nfi_server *server, struct nfi_info *inf);
 
 
 #endif
+
