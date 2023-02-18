cd9107fad1264c0c6f35624c8cae8631f1f023e2
Alejandro <acaldero@inf.uc3m.es>
2023-02-18 14:55:00 +0100

    2.1: minor updates

--- doc/spack.py
+++ doc/spack.py
@@ -3,57 +3,41 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# You can save this file and test your package like this:
-#     spack install xpn
-#
-# You can edit this file again by typing:
-#     spack edit xpn
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
 
 class Xpn(AutotoolsPackage):
-    """XPN Parallel File System"""
+    """The Expand Parallel File System (XPN)"""
 
     # Package's homepage here.
     homepage = "https://github.com/xpn-arcos/xpn/"
-   #url = "https://github.com/xpn-arcos/xpn/archive/refs/tags/v2.0.0.tar.gz"
+   #url = "https://github.com/xpn-arcos/xpn/archive/refs/tags/v2.1.0.tar.gz"
     url = "https://github.com/acaldero/xpn/archive/refs/tags/v2.1.tar.gz"
 
     # List of GitHub accounts to notify when the package is updated.
-    maintainers = ["dcamarmas", "acaldero"]
+    maintainers = ["dcamarmas", "acaldero", "fgcarbal"]
 
-   #version("2.0.0", sha256="affa2b72d2671c90aefb8cff47ff884affdd471cab0be7124c1ac99728c6afb6")
-    version("2.1",   sha256="e1a732c7318a94b9b53d984e8d7bddb530c1f70565a94e111f18b3a21d9a4f21")
+    version("2.1", sha256="25a15aa2b2a7bb57f457f171500d3fa1b72752e02e7daa420a8f8033518023f5")
 
     # Variants
     variant("mpich", default=False, description="Use MPICH")
 
     # Dependencies
     depends_on("mxml")
-    depends_on("mpich",    when="+mpich")
+    depends_on("mpich",                  when="+mpich")
     depends_on('autoconf', type='build', when='@master')
     depends_on('automake', type='build', when='@master')
     depends_on('libtool',  type='build', when='@master')
 
-    #def configure_args(self):
-    #    args = []
-    #    return args
 
     def install(self, spec, prefix):
-        # ACLOCAL_FLAGS="-I /usr/share/aclocal/" autoreconf -v -i -s -W all
-
-        # ./configure --prefix=$INSTALL_PATH/xpn --enable-tcp_server --enable-mpi_server="$MPICC_PATH"
+        ## ./configure --prefix=$INSTALL_PATH/xpn --enable-tcp_server --enable-mpi_server="$MPICC_PATH"
         configure("--prefix=" + prefix,
                   "--enable-tcp_server",
-                  "--enable-mpi_server="  + spec['mpicc_path'].prefix)
+                  "--enable-mpi_server")
 
-        # make clean
-        # make -j 8
+        ## make -j
         make()
 
         # make install
