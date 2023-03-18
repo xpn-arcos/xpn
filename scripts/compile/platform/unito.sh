35c5ea22abd262871eaf06cb0f44b5786d074040
Diego Camarmas <dcamarma@inf.uc3m.es>
2023-03-18 14:27:49 +0100

    Update unito.sh

--- scripts/compile/platform/unito.sh
+++ scripts/compile/platform/unito.sh
@@ -27,8 +27,8 @@
 spack load mpich
 
 # 2) working path...
-MPICC_PATH=$HOME/opt/spack/linux-ubuntu20.04-zen/gcc-9.4.0/openmpi-4.1.3-4bpvwm3lcbftmjki6en35c4i5od6wjbr/bin/mpicc
-#MPICC_PATH=$HOME/opt/spack/linux-ubuntu20.04-zen/gcc-9.4.0/mpich-4.0.2-a76rmlxbneoqdvemzjsyewp2akiiuxlj/bin/mpicc
+#MPICC_PATH=$HOME/opt/spack/linux-ubuntu20.04-zen/gcc-9.4.0/openmpi-4.1.3-4bpvwm3lcbftmjki6en35c4i5od6wjbr/bin/mpicc
+MPICC_PATH=$HOME/opt/spack/linux-ubuntu20.04-zen/gcc-9.4.0/mpich-4.0.2-a76rmlxbneoqdvemzjsyewp2akiiuxlj/bin/mpicc
 INSTALL_PATH=$HOME/dcamarma/bin/
 BASE_PATH=$(dirname $0)
 
