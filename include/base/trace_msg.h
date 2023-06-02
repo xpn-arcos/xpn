0fd0b4597a231193b3980910a249442f8984e64c
cristhma <cristhma@pa.uc3m.es>
2023-06-02 16:26:30 +0200

    2.3.0: initial support for prom.h - doxygen

--- include/base/trace_msg.h
+++ include/base/trace_msg.h
@@ -1,6 +1,7 @@
 
 /*
- *  Copyright 2000-2023 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos, Luis Miguel Sanchez Garcia, Borja Bergua Guerra
+ *  Copyright 2000-2023 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos, Luis Miguel Sanchez
+ * Garcia, Borja Bergua Guerra
  *
  *  This file is part of Expand.
  *
@@ -25,7 +26,8 @@
  *
  * Header file to 'TODO'.
  *
- * @authors Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos, Luis Miguel Sanchez Garcia, Borja Bergua Guerra
+ * @authors Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos, Luis Miguel Sanchez Garcia,
+ * Borja Bergua Guerra
  * @date  Jul 22, 2021
  * @bug No known bugs.
  */
@@ -34,117 +36,143 @@
 #define TRACE_MSG_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-  /************************************************
-   *  ... Includes
-   ***********************************************/
+/************************************************
+ *  ... Includes
+ ***********************************************/
 #include "all_system.h"
 #include "string_misc.h"
 
 #ifdef HAVE_PROM_H
+#include "microhttpd.h"
 #include "prom.h"
 #include "promhttp.h"
-#include "microhttpd.h"
 #endif
 
-  /************************************************
-   *  ... Constants
-   ***********************************************/
+/************************************************
+ *  ... Constants
+ ***********************************************/
 
 #define TRACE_PRINTER_SIZE 8
 
-#define SPOT __LINE__, __FILE__, getpid(), 0
+#define SPOT               __LINE__, __FILE__, getpid(), 0
 
-  /************************************************
-   *  ... Functions
-   ***********************************************/
+/************************************************
+ *  ... Functions
+ ***********************************************/
 
-  /**
-   * @brief 'TODO'.
-   *
-   * 'TODO'.
-   *
-   * @par Parameters
-   *    None.
-   * @par Returns
-   *    Nothing.
-   */
-  int  base_trace_msg_init(void);
+/**
+ * @brief 'TODO'.
+ *
+ * 'TODO'.
+ *
+ * @par Parameters
+ *    None.
+ * @par Returns
+ *    Nothing.
+ */
+int base_trace_msg_init(void);
 
-  /**
-   *  @brief Set 'printer' dispacher.
-   *
-   *  @param index   index of the printer, from zero up to TRACE_PRINTER_SIZE-1
-   *  @param printer the printer function to be used.
-   *  @par Returns
-   *    Nothing.
-   */
-  int base_trace_msg_set_printer(
-      /*IN*/ int index,
-      /*IN*/ int (*printer)(const char *, va_list));
+/**
+ *  @brief Set 'printer' dispacher.
+ *
+ *  @param index   index of the printer, from zero up to TRACE_PRINTER_SIZE-1
+ *  @param printer the printer function to be used.
+ *
+ *  @return 'TODO'
+ */
+int base_trace_msg_set_printer(
+    /*IN*/ int index,
+    /*IN*/ int (*printer)(const char *, va_list));
 
-  /**
-   *  @brief Write a message.
-   *
-   *  Writes a message, using a variable format and argument list.
-   *
-   *  @param line the line of code where message is generated.
-   *  @param name the file name at the code where message is generated.
-   *  @param pid  the process that send this message.
-   *  @param type the type of message.
-   *  @param fto  the message format.
-   *  @param vl   the argument list.
-   *  @par Returns
-   *    Nothing.
-   */
-  void base_trace_msg_variable_printf(
-      /*IN*/ int line,
-      /*IN*/ char *name,
-      /*IN*/ long pid,
-      /*IN*/ int type,
-      /*IN*/ char *fto,
-      /*IN*/ va_list vl);
+/**
+ *  @brief Write a message.
+ *
+ *  Writes a message, using a variable format and argument list.
+ *
+ *  @param line the line of code where message is generated.
+ *  @param name the file name at the code where message is generated.
+ *  @param pid  the process that send this message.
+ *  @param type the type of message.
+ *  @param fto  the message format.
+ *  @param vl   the argument list.
+ *  @par Returns
+ *    Nothing.
+ */
+void base_trace_msg_variable_printf(
+    /*IN*/ int line,
+    /*IN*/ char *name,
+    /*IN*/ long pid,
+    /*IN*/ int type,
+    /*IN*/ char *fto,
+    /*IN*/ va_list vl);
 
-  /**
-   *  @brief Write a message.
-   *
-   *  Write a message using the format and arguments given to it (using PrintMsg).
-   *
-   *  @param line the line of code where message is generated.
-   *  @param name the file name at the code where message is generated.
-   *  @param pid  the process that send this message.
-   *  @param type the type of message.
-   *  @param fto  the message format.
-   *  @par Returns
-   *    Nothing.
-   */
-  void base_trace_msg_printf(
-      /*IN*/ int line,
-      /*IN*/ char *name,
-      /*IN*/ long pid,
-      /*IN*/ int type,
-      /*IN*/ char *fto,
-      ...);
+/**
+ *  @brief Write a message.
+ *
+ *  Write a message using the format and arguments given to it (using PrintMsg).
+ *
+ *  @param line the line of code where message is generated.
+ *  @param name the file name at the code where message is generated.
+ *  @param pid  the process that send this message.
+ *  @param type the type of message.
+ *  @param fto  the message format.
+ *  @par Returns
+ *    Nothing.
+ */
+void base_trace_msg_printf(
+    /*IN*/ int line,
+    /*IN*/ char *name,
+    /*IN*/ long pid,
+    /*IN*/ int type,
+    /*IN*/ char *fto, ...);
 
+/**
+ * @brief 'TODO'.
+ *
+ * 'TODO'.
+ *
+ * @par Parameters
+ *    None.
+ *
+ *  @return 'TODO'
+ */
+int base_trace_prom_init(void);
 
-  int base_trace_prom_init     ( void ) ;
-  int base_trace_prom_finalize ( void ) ;
-  int base_trace_msg_bar       ( int v, const char *label ) ;
+/**
+ * @brief 'TODO'.
+ *
+ * 'TODO'.
+ *
+ * @par Parameters
+ *    None.
+ *
+ *  @return 'TODO'
+ */
+int base_trace_prom_finalize(void);
 
+/**
+ * @brief 'TODO'.
+ *
+ * 'TODO'.
+ *
+ *  @param v the type of message.
+ *  @param label  the message format.
+ *
+ *  @return 'TODO'
+ */
+int base_trace_msg_bar(int v, const char *label);
 
-  /************************************************
-   *  ... Macros
-   ***********************************************/
-#define DEBUG_BEGIN() \
-  debug_info("Begin %s()\n", __func__)
+/************************************************
+ *  ... Macros
+ ***********************************************/
+#define DEBUG_BEGIN() debug_info("Begin %s()\n", __func__)
 
-#define DEBUG_END() \
-  debug_info("End   %s(), errno=%d\n", __func__, errno)
+#define DEBUG_END()   debug_info("End   %s(), errno=%d\n", __func__, errno)
 
-  /* ................................................................... */
+/* ................................................................... */
 
 #ifdef __cplusplus
 }
