7784e0a34105be09029b3e9b256fd707d116444d
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2023-11-25 13:42:31 +0100

    2.2.2: minor updates

--- test/integrity/bypass_c/open-write-close.c
+++ test/integrity/bypass_c/open-write-close.c
@@ -1,6 +1,5 @@
 
 #include "all_system.h"
-#include "xpn.h"
 #include <sys/time.h>
 
 
@@ -29,27 +28,21 @@ int main ( int argc, char *argv[] )
 	    printf(" Usage: %s <full path> <megabytes to write>\n", argv[0]) ;
 	    printf("\n") ;
 	    printf(" Example:") ;
-	    printf(" env XPN_CONF=./xpn.conf XPN_DNS=/shared/tcp_server.dns %s /P1/test_1 2\n", argv[0]);
+	    printf(" %s /tmp/expand/test_1 2\n", argv[0]);
 	    printf("\n") ;
 	    return -1 ;
 	}	
 
-	// xpn-init
-	ret = xpn_init();
-	printf("%d = xpn_init()\n", ret);
-	if (ret < 0) {
-	    return -1;
-	}
-
+	// init
 	memset(buffer, 'a', BUFF_SIZE) ;
-	printf("memset(buffer, 'a', %d)\n", BUFF_SIZE) ;
+     // printf("memset(buffer, 'a', %d)\n", BUFF_SIZE) ;
 
 	// xpn-creat
 	t_bc = get_time();
 
-	fd1 = xpn_creat(argv[1], 00777);
+	fd1 = creat(argv[1], 00777);
 	if (fd1 < 0) {
-	    printf("%d = xpn_creat('%s', %o)\n", ret, argv[1], 00777) ;
+	    printf("%d = creat('%s', %o)\n", ret, argv[1], 00777) ;
 	    return -1 ;
 	}
 
@@ -59,26 +52,19 @@ int main ( int argc, char *argv[] )
         long mb = atoi(argv[2]) ;
 	for (int i = 0; i < mb; i++)
 	{
-	     ret = xpn_write(fd1, buffer, BUFF_SIZE);
-	  // printf("%d = xpn_write_%d(%d, %p, %lu)\n", ret, i, fd1, buffer, (unsigned long)BUFF_SIZE);
+	     ret = write(fd1, buffer, BUFF_SIZE);
+	  // printf("%d = write_%d(%d, %p, %lu)\n", ret, i, fd1, buffer, (unsigned long)BUFF_SIZE);
 	}
 	
 	t_aw = get_time() - t_bw;
 
-	ret = xpn_close(fd1);
-     // printf("%d = xpn_close(%d)\n", ret, fd1) ;
+	ret = close(fd1);
+     // printf("%d = close(%d)\n", ret, fd1) ;
 
 	t_ac = get_time() - t_bc;
 
 	printf("Bytes; Total time (ms); Write time (ms)\n") ;
-	printf("%f;%f;%f\n", (double)mb * BUFF_SIZE, t_ac * 1000, t_aw * 1000) ;
-
-	// xpn-destroy
-	ret = xpn_destroy();
-	printf("%d = xpn_destroy()\n", ret);
-	if (ret < 0) {
-	    return -1;
-	}
+	printf("%f;%f;%f\n", (double)mb * (double)BUFF_SIZE, t_ac * 1000, t_aw * 1000) ;
 
 	return 0;
 }
