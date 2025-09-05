8f5343bd8547302b090c3cf315014adc764af9af
Alejandro Calderon Mateos <acaldero@inf.uc3m.es>
2025-09-05 19:09:41 +0200

    3.3.1: minor updates

--- test/performance/xpn/open-read-close.c
+++ test/performance/xpn/open-read-close.c
@@ -1,4 +1,25 @@
 
+/*
+ *  Copyright 2020-2025 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos, Elías Del Pozo Puñal
+ *
+ *  This file is part of Expand.
+ *
+ *  Expand is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Expand is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Expand.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include "all_system.h"
 #include "xpn.h"
 #include <sys/time.h>
@@ -22,8 +43,9 @@ int main ( int argc, char *argv[] )
 {
 	int    ret, fd1 ;
 	double t_bc, t_ac, t_bw, t_aw ;
+	long   mb ;
 
-    if (argc < 3)
+        if (argc < 3)
 	{
 	    printf("\n") ;
 	    printf(" Usage: %s <full path> <megabytes to read>\n", argv[0]) ;
@@ -35,11 +57,11 @@ int main ( int argc, char *argv[] )
 	}	
 
 	// xpn-init
-	/*ret = xpn_init();
+	ret = xpn_init();
 	printf("%d = xpn_init()\n", ret);
 	if (ret < 0) {
 	    return -1;
-	}*/
+	}
 
 	memset(buffer, 'a', BUFF_SIZE) ;
 	printf("memset(buffer, 'a', %d)\n", BUFF_SIZE) ;
@@ -56,7 +78,7 @@ int main ( int argc, char *argv[] )
 	t_bw = get_time();
 
 	// xpn-write
-    long mb = atoi(argv[2]) ;
+        mb = atoi(argv[2]) ;
 	for (int i = 0; i < mb; i++)
 	{
 		ret = xpn_read(fd1, buffer, BUFF_SIZE);
@@ -74,12 +96,12 @@ int main ( int argc, char *argv[] )
 	printf("%f;%f;%f\n", (double)mb * BUFF_SIZE, t_ac * 1000, t_aw * 1000) ;
 
 	// xpn-destroy
-	/*ret = xpn_destroy();
+	ret = xpn_destroy();
 	printf("%d = xpn_destroy()\n", ret);
 	if (ret < 0) {
 	    return -1;
 	}
-*/
+
 	return 0;
 }
 
