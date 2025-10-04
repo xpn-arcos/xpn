124d520afcd511fd921fa2c5454a208789e9e975
Alejandro Calderon Mateos <acaldero@inf.uc3m.es>
2025-10-04 03:04:29 +0200

    3.3.1: minor updates on xpn_proxy_server

--- src/base/service_socket.c
+++ src/base/service_socket.c
@@ -27,105 +27,104 @@
 
   /* ... Functions / Funciones ......................................... */
 
-int sersoc_lookup_port_name ( char * srv_name, char * port_name, int socket_accept_code )
-{
-    int ret = -1 ;
+     int sersoc_lookup_port_name ( char * srv_name, char * port_name, int socket_accept_code )
+     {
+         int ret = -1 ;
 
-    // Lookup port name
-    debug_info("[SERSOC] [sersoc_lookup_port_name] >> Begin\n");
+         // Lookup port name
+         debug_info("[SERSOC] [sersoc_lookup_port_name] >> Begin\n");
 
-    int port = utils_getenv_int("XPN_SCK_PORT", DEFAULT_XPN_SCK_PORT) ;
-    ret = sersoc_do_send_recv(srv_name, port, socket_accept_code, port_name) ;
+         int port = utils_getenv_int("XPN_SCK_PORT", DEFAULT_XPN_SCK_PORT) ;
+         ret = sersoc_do_send_recv(srv_name, port, socket_accept_code, port_name) ;
 
-    debug_info("[SERSOC] [sersoc_lookup_port_name] << End\n");
-    return ret;
-}
+         debug_info("[SERSOC] [sersoc_lookup_port_name] << End\n");
+         return ret;
+     }
 
-int sersoc_do_send_recv ( char * srv_name, int port, int req_id, char *res_val )
-{
-    int ret = -1 ;
-    int connection_socket ;
+     int sersoc_do_send_recv ( char * srv_name, int port, int req_id, char *res_val )
+     {
+         int ret = -1 ;
+         int connection_socket ;
 
-    debug_info("[SERSOC] [sersoc_do_send_recv] >> Begin\n");
+         debug_info("[SERSOC] [sersoc_do_send_recv] >> Begin\n");
+
+         int ipv = utils_getenv_int("XPN_SCK_IPV", DEFAULT_XPN_SCK_IPV);
+
+         // Lookup port name
+         ret  = socket_client_connect(srv_name, port, &connection_socket, ipv);
+         if (ret < 0)
+         {
+             debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket connect\n");
+             return -1;
+         }
+
+         ret = socket_send(connection_socket, &req_id, sizeof(int));
+         if (ret < 0)
+         {
+             debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket send\n");
+             socket_close(connection_socket);
+             return -1;
+         }
+
+         ret = socket_recv(connection_socket, res_val, MAX_PORT_NAME_LENGTH);
+         if (ret < 0)
+         {
+             debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket read\n");
+             socket_close(connection_socket);
+             return -1;
+         }
+
+         ret = socket_close(connection_socket);
+         if (ret < 0)
+         {
+             debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket close\n");
+             return -1;
+         }
+
+         debug_error("[SERSOC] [sersoc_do_send_recv] request to '%s' command '%d' -> response: %s\n", srv_name, req_id, res_val);
+         debug_info("[SERSOC] [sersoc_do_send_recv] << End\n");
+
+         return ret;
+     }
+
+     int sersoc_do_send ( char * srv_name, int port, int req_id )
+     {
+         int ret = -1 ;
+         int connection_socket ;
+
+         debug_info("[SERSOC] [sersoc_do_send] >> Begin\n");
+
+         int ipv = utils_getenv_int("XPN_SCK_IPV", DEFAULT_XPN_SCK_IPV);
+
+         // Lookup port name
+         ret  = socket_client_connect(srv_name, port, &connection_socket, ipv);
+         if (ret < 0)
+         {
+             debug_error("[SERSOC] [sersoc_do_send] ERROR: socket connect\n");
+             return -1;
+         }
+
+         ret = socket_send(connection_socket, &req_id, sizeof(int));
+         if (ret < 0)
+         {
+             debug_error("[SERSOC] [sersoc_do_send] ERROR: socket send\n");
+             socket_close(connection_socket);
+             return -1;
+         }
+
+         ret = socket_close(connection_socket);
+         if (ret < 0)
+         {
+             debug_error("[SERSOC] [sersoc_do_send] ERROR: socket close\n");
+             return -1;
+         }
+
+         debug_error("[SERSOC] [sersoc_do_send] request to '%s' command '%d' -> response: %s\n", srv_name, req_id, res_val);
+         debug_info("[SERSOC] [sersoc_do_send] << End\n");
+
+         return ret;
+     }
 
 
-    int ipv = utils_getenv_int("XPN_SCK_IPV", DEFAULT_XPN_SCK_IPV);
-
-    // Lookup port name
-    ret  = socket_client_connect(srv_name, port, &connection_socket, ipv);
-    if (ret < 0)
-    {
-        debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket connect\n");
-        return -1;
-    }
-
-    ret = socket_send(connection_socket, &req_id, sizeof(int));
-    if (ret < 0)
-    {
-        debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket send\n");
-        socket_close(connection_socket);
-        return -1;
-    }
-
-    ret = socket_recv(connection_socket, res_val, MAX_PORT_NAME_LENGTH);
-    if (ret < 0)
-    {
-        debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket read\n");
-        socket_close(connection_socket);
-        return -1;
-    }
-
-    ret = socket_close(connection_socket);
-    if (ret < 0)
-    {
-        debug_error("[SERSOC] [sersoc_do_send_recv] ERROR: socket close\n");
-        return -1;
-    }
-
-    debug_error("[SERSOC] [sersoc_do_send_recv] request to '%s' command '%d' -> response: %s\n", srv_name, req_id, res_val);
-    debug_info("[SERSOC] [sersoc_do_send_recv] << End\n");
-
-    return ret;
-}
-
-int sersoc_do_send ( char * srv_name, int port, int req_id )
-{
-    int ret = -1 ;
-    int connection_socket ;
-
-    debug_info("[SERSOC] [sersoc_do_send] >> Begin\n");
-
-    int ipv = utils_getenv_int("XPN_SCK_IPV", DEFAULT_XPN_SCK_IPV);
-
-    // Lookup port name
-    ret  = socket_client_connect(srv_name, port, &connection_socket, ipv);
-    if (ret < 0)
-    {
-        debug_error("[SERSOC] [sersoc_do_send] ERROR: socket connect\n");
-        return -1;
-    }
-
-    ret = socket_send(connection_socket, &req_id, sizeof(int));
-    if (ret < 0)
-    {
-        debug_error("[SERSOC] [sersoc_do_send] ERROR: socket send\n");
-        socket_close(connection_socket);
-        return -1;
-    }
-
-    ret = socket_close(connection_socket);
-    if (ret < 0)
-    {
-        debug_error("[SERSOC] [sersoc_do_send] ERROR: socket close\n");
-        return -1;
-    }
-
-    debug_error("[SERSOC] [sersoc_do_send] request to '%s' command '%d' -> response: %s\n", srv_name, req_id, res_val);
-    debug_info("[SERSOC] [sersoc_do_send] << End\n");
-
-    return ret;
-}
-
-
-/* ................................................................... */
+  /* ................................................................... */
 
